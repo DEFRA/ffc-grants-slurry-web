f547cef8bac42d4544287b9672b6ae6bcc829851
ogbk <ogbu.k.olu@gmail.com>
2022-06-21 17:58:14 +0100

    test app-insights.test -> logException()

--- test/integration/narrow/app/app-insights.test.js
+++ test/integration/narrow/app/app-insights.test.js
@@ -1,40 +1,57 @@
 describe('App Insights', () => {
-    test('setup', () => {
-      let mockAppInsights = jest.mock('applicationinsights').fn(() => ({
-        defaultClient: {
-					context: {
-						keys: {
-          		cloudRole: 'MOCK_CLOUDROLE',
-						},
-						tags: {}
-					}
-				},
-				setup: jest.fn(() => ({ start: jest.mock().fn() }))
-      }))
-
-			let mockConfig = jest.mock('../../../../app/config/server').fn(() => ({
-				appInsights: {
-					key: 'MOCK_KEY',
-					role: 'MOCK_ROLE'
+	test('setup', () => {
+		let mockAppInsights = jest.mock('applicationinsights').fn(() => ({
+			defaultClient: {
+				context: {
+					keys: {
+						cloudRole: 'MOCK_CLOUDROLE',
+					},
+					tags: {}
 				}
-			}))
+			},
+			setup: jest.fn(() => ({ start: jest.mock().fn() }))
+		}))
 
-			let appInsights = mockAppInsights()
-			let config = mockConfig()
+		let mockConfig = jest.mock('../../../../app/config/server').fn(() => ({
+			appInsights: {
+				key: 'MOCK_KEY',
+				role: 'MOCK_ROLE'
+			}
+		}))
 
-			require('../../../../app/services/app-insights').setup()
+		let appInsights = mockAppInsights()
+		let config = mockConfig()
 
-			expect(appInsights).toBeDefined()
-			expect(config).toBeDefined()
-			expect(appInsights.setup).toBeDefined()
-			expect(appInsights.setup().start).toBeDefined()
-			expect (appInsights.defaultClient.context.keys.cloudRole).toBe('MOCK_CLOUDROLE')
-			expect (config.appInsights.key).toBe('MOCK_KEY')
-			expect (config.appInsights.role).toBe('MOCK_ROLE')
+		require('../../../../app/services/app-insights').setup()
 
-			mockAppInsights.mockRestore()
-			mockConfig.mockRestore()
+		expect(appInsights).toBeDefined()
+		expect(config).toBeDefined()
+		expect(appInsights.setup).toBeDefined()
+		expect(appInsights.setup().start).toBeDefined()
+		expect (appInsights.defaultClient.context.keys.cloudRole).toBe('MOCK_CLOUDROLE')
+		expect (config.appInsights.key).toBe('MOCK_KEY')
+		expect (config.appInsights.role).toBe('MOCK_ROLE')
 
-    })
+		mockAppInsights.mockRestore()
+		mockConfig.mockRestore()
+
+	})
+
+	test('log exception', () => {
+		let mockAppInsights = jest.mock('applicationinsights').fn(() => ({
+			defaultClient: {
+				context: {
+					keys: {
+						cloudRole: 'MOCK_CLOUDROLE',
+					},
+					tags: {}
+				}
+			}
+		}))
+
+		let appInsights = mockAppInsights()
+		expect(appInsights.defaultClient).toBeDefined()
+		expect(appInsights.defaultClient.trackException).toBeUndefined()
+	})
 })
   
