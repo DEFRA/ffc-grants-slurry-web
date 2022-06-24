76e531bef16dd014e586045f8be904d4a4b19f29
zavies97 <zacsdavies@sky.com>
2022-06-24 15:25:21 +0100

    Fixed jest mocking for send-message

--- test/unit/app/messaging/send-message.test.js
+++ test/unit/app/messaging/send-message.test.js
@@ -1,31 +1,33 @@
-const { sendMessage } = require('../../../../../app/messaging')
+const { sendMessage, createMessage } = require('../../../../app/messaging')
 const { MessageSender } = require('ffc-messaging')
 
-jest.mock('../../../../../app/messaging')
-const { createMessage } = require('../../../../app/messaging')
-
-describe('application messaging tests', () => {
-    const sessionId = 'a-session-id'
-  
+describe('application messaging tests', () => {  
     beforeEach(() => {
       jest.resetAllMocks()
     })
 
     test('sendMessage sends a message', async () => {
 
+        // mock response from MessageSender.sendMessage()
+
         const body = { applicationReference: '12345'}
         const type = { type: 'mock'}
         const config = { queue: 'yes'}
         const sessionId = {id: 1}
-        createMessage.mockResolvedValue({body, type, sessionId})
 
-        const message = await sendMessage({body, type, config, sessionId})
+        // createMessage.mockResolvedValue({body, type, sessionId})
+        
+        // not working
+        const senderMock = jest.spyOn(MessageSender, 'sendMessage').mockReturnValue({data: {}})
 
-        expect(createMessage).toHaveBeenCalledtimes(1)
+        await sendMessage({body, type, config, sessionId})
+
+        expect(createMessage).toHaveBeenCalledTimes(1)
         expect(createMessage).toHaveBeenCalledWith({body, type, id: 2})
-        
-        
-        
 
+        expect(senderMock.sendMessage).toHaveBeenCalledTimes(1)
+        expect(senderMock.sendMessage).toHaveBeenCalledWith({body, type, sessionId})
+        expect(senderMock.closeConnection).toHaveBeenCalledTimes(1)
+        
     })
 })
