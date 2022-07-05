417cf4ec28320aa6372f184c60f2e452e3796902
yunusfirat <yunusfirat44@gmail.com>
2022-07-05 11:34:38 +0100

    added id to back link test.

--- test/integration/narrow/routes/planning-permission-condition.test.js
+++ test/integration/narrow/routes/planning-permission-condition.test.js
@@ -40,6 +40,6 @@ it('page loads with correct back link', async () => {
     }
     const response = await global.__SERVER__.inject(options)
     expect(response.statusCode).toBe(200)
-    expect(response.payload).toContain('<a href=\"planning-permission\" class=\"govuk-back-link\">Back</a>')
+    expect(response.payload).toContain('<a href=\"planning-permission\" class=\"govuk-back-link\" id=\"linkBack\">Back</a>')
 })
 })
