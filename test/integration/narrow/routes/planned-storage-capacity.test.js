402d5f74daadff59a4d82877e415b9746fd03239
yunusfirat <yunusfirat44@gmail.com>
2022-07-05 09:37:25 +0100

    version increment

--- test/integration/narrow/routes/planned-storage-capacity.test.js
+++ test/integration/narrow/routes/planned-storage-capacity.test.js
@@ -61,14 +61,4 @@ describe('Page: /planned-storage-capacity', () => {
     expect(postResponse.statusCode).toBe(302)
     expect(postResponse.headers.location).toBe('project-type')
   })
-
-//   it('page loads with correct back link', async () => {
-//     const options = {
-//     method: 'GET',
-//     url: `${global.__URLPREFIX__}/planned-storage-capacity`
-//     }
-//     const response = await global.__SERVER__.inject(options)
-//     expect(response.statusCode).toBe(200)
-//     expect(response.payload).toContain(`<a href=\"existing-storage-capacity\" class=\"govuk-back-link\">Back</a>`)
-//     })
 })
