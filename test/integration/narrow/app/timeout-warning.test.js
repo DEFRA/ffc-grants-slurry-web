fc4475d782f11c0155005dccd920b1e74137b6cd
ogbk <ogbu.k.olu@gmail.com>
2022-07-07 17:44:31 +0100

    complete test on constructor function

--- test/integration/narrow/app/timeout-warning.test.js
+++ test/integration/narrow/app/timeout-warning.test.js
@@ -1,41 +1,46 @@
+// mock dialog-polyfill
 jest.mock('dialog-polyfill', () => ({
   forceRegisterDialog: jest.fn(() => 2),
   registerDialog: jest.fn(() => 4)
 }))
 const dialogPolyfill = require('dialog-polyfill')
 
-const TimeoutWarning = require('../../../../app/templates/components/timeout-warning/timeout-warning')
-
+// mock module object - parameter of constructor TimeoutWarning
 const mockModule = {
   querySelector: jest.fn((param) => (`mqs_${param}`)),
   getAttribute: jest.fn((param) => null)
 }
 
+// mock document & window - DOM global values
 const { JSDOM } = require('jsdom')
 const dom = new JSDOM()
 global.document = dom.window.document
 global.window = dom.window
 
+// import TimeoutWarning
+const TimeoutWarning = require('../../../../app/templates/components/timeout-warning/timeout-warning')
+
 describe('Timeout Warning', () => {
   it('test TimeoutWarning constructor', () => {
     expect(TimeoutWarning).toBeDefined()
-    expect(new TimeoutWarning(mockModule)).toEqual(
-      expect.objectContaining({
-        $module: mockModule,
-        $lastFocusedEl: null,
-        $closeButton: 'mqs_.js-dialog-close',
-        $cancelButton: 'mqs_.js-dialog-cancel',
-        overLayClass: 'govuk-timeout-warning-overlay',
-        // $fallBackElement: document.querySelector('.govuk-timeout-warning-fallback'),
-        timers: [],
-        $countdown: 'mqs_.timer',
-        $accessibleCountdown: 'mqs_.at-timer',
-        idleMinutesBeforeTimeOut: 25,
-        timeOutRedirectUrl: 'timeout',
-        minutesTimeOutModalVisible: 5,
-        timeUserLastInteractedWithPage: ''
-      })
-    )
+
+    jest.spyOn(document, 'querySelector').mockImplementation((param) => (`dqs_${param}`))
+
+    expect(new TimeoutWarning(mockModule)).toEqual({
+      $module: mockModule,
+      $lastFocusedEl: null,
+      $closeButton: 'mqs_.js-dialog-close',
+      $cancelButton: 'mqs_.js-dialog-cancel',
+      overLayClass: 'govuk-timeout-warning-overlay',
+      $fallBackElement: 'dqs_.govuk-timeout-warning-fallback',
+      timers: [],
+      $countdown: 'mqs_.timer',
+      $accessibleCountdown: 'mqs_.at-timer',
+      idleMinutesBeforeTimeOut: 25,
+      timeOutRedirectUrl: 'timeout',
+      minutesTimeOutModalVisible: 5,
+      timeUserLastInteractedWithPage: ''
+    })
 
     mockModule.getAttribute.mockImplementation((param) => {
       switch (param) {
