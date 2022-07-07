6500f9fe4086025535bb4a80882750f0e7077fb3
yunusfirat <yunusfirat44@gmail.com>
2022-07-07 14:29:10 +0100

    planning-permission-summary page update

--- app/config/question-bank.js
+++ app/config/question-bank.js
@@ -1026,44 +1026,23 @@ const questionBank = {
           key: 'planning-permission-summary',
           order: 153,
           title: 'Check your answers before getting your results',
+          pageTitle: 'planning-permission-summary',
           url: 'planning-permission-summary',
           baseUrl: 'planning-permission-summary',
           backUrl: 'grid-reference',
           nextUrl: 'result-page',
-          type: 'single-answer',
-          minAnswerCount: 1,
-          sidebar: {
-            values: [{
-              heading: 'Eligibility',
-              content: [{
-                para: `Any planning permission must be in place by 31 December 2023. 
-
-                      You must have applied for planning permission before you submit a full application.`
-              }]
-            }]
+          preValidationKeys: [],
+          eliminationAnswerKeys: '',
+          ineligibleContent: {},
+          pageData: {
+            planningPermissionLink: 'planning-permission',
+            planningPermissionEvidenceLink: 'planning-permission-evidence',
+            gridReferenceLink: 'grid-reference'
           },
-          validate: [
-            {
-              type: 'NOT_EMPTY',
-              error: 'Select your project planning permission'
-            }
-          ],
-          answers: [
-            {
-              key: 'planning-permission-A1',
-              value: 'Approved'
-            },
-            {
-              key: 'planning-permission-A2',
-              value: 'Applied for but not yet approved'
-            },
-            {
-              key: 'planning-permission-A3',
-              value: 'Not yet applied for but expected to be in place by 31 December 2023',
-              redirectUrl: 'planning-permission-condition'
-            }
-          ],
-          yarKey: 'PlanningPermissionSummary'
+          fundingPriorities: '',
+          type: '',
+          minAnswerCount: 1,
+          answers: []
         },
         {
           key: 'result-page',
