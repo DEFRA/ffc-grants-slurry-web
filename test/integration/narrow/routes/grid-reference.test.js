3aaa97b9592a7b5f2c1d9e71344cc9dfe0e29307
ogbk <ogbu.k.olu@gmail.com>
2022-07-17 23:29:59 +0100

    automatic code linting

--- test/integration/narrow/routes/grid-reference.test.js
+++ test/integration/narrow/routes/grid-reference.test.js
@@ -1,86 +1,86 @@
 const { crumbToken } = require('./test-helper')
 
 describe('Page: /grid-reference', () => {
-const varList = { 'current-score': 'randomData' }
+  const varList = { 'current-score': 'randomData' }
 
-jest.mock('../../../../app/helpers/session', () => ({
+  jest.mock('../../../../app/helpers/session', () => ({
     setYarValue: (request, key, value) => null,
     getYarValue: (request, key) => {
-    if (varList[key]) return varList[key]
-    else return 'Error'
+      if (varList[key]) return varList[key]
+      else return 'Error'
     }
-}))
+  }))
 
-it('page loads successfully, with all the options', async () => {
+  it('page loads successfully, with all the options', async () => {
     const options = {
-    method: 'GET',
-    url: `${global.__URLPREFIX__}/grid-reference`
+      method: 'GET',
+      url: `${global.__URLPREFIX__}/grid-reference`
     }
 
     const response = await global.__SERVER__.inject(options)
     expect(response.statusCode).toBe(200)
     expect(response.payload).toContain('What is the OS grid reference for your slurry store?')
     expect(response.payload).toContain('OS grid reference number')
-})
+  })
 
-it('no option selected -> show error message', async () => {
+  it('no option selected -> show error message', async () => {
     const postOptions = {
-    method: 'POST',
-    url: `${global.__URLPREFIX__}/grid-reference`,
-    headers: { cookie: 'crumb=' + crumbToken },
-    payload: { PlanningPermissionEvidence: '', crumb: crumbToken }
+      method: 'POST',
+      url: `${global.__URLPREFIX__}/grid-reference`,
+      headers: { cookie: 'crumb=' + crumbToken },
+      payload: { PlanningPermissionEvidence: '', crumb: crumbToken }
     }
 
     const postResponse = await global.__SERVER__.inject(postOptions)
     expect(postResponse.statusCode).toBe(200)
     expect(postResponse.payload).toContain('Enter OS Grid reference')
-})
+  })
 
-it('should validate OS grid reference number - OS Grid Reference must be two letters followed by 8 digits', async () => {
+  it('should validate OS grid reference number - OS Grid Reference must be two letters followed by 8 digits', async () => {
     const postOptions = {
-    method: 'POST',
-    url: `${global.__URLPREFIX__}/grid-reference`,
-    headers: { cookie: 'crumb=' + crumbToken },
-    payload: {
+      method: 'POST',
+      url: `${global.__URLPREFIX__}/grid-reference`,
+      headers: { cookie: 'crumb=' + crumbToken },
+      payload: {
         gridReferenceNumber: 'ds123456',
         crumb: crumbToken
-    }
+      }
     }
 
     const postResponse = await global.__SERVER__.inject(postOptions)
     expect(postResponse.statusCode).toBe(200)
     expect(postResponse.payload).toContain('OS Grid Reference must be two letters followed by 8 digits')
-})
+  })
 
-it('should validate OS grid reference number - First two characters should be letter following eight characters must be numbers', async () => {
+  it('should validate OS grid reference number - First two characters should be letter following eight characters must be numbers', async () => {
     const postOptions = {
-    method: 'POST',
-    url: `${global.__URLPREFIX__}/grid-reference`,
-    headers: { cookie: 'crumb=' + crumbToken },
-    payload: {
+      method: 'POST',
+      url: `${global.__URLPREFIX__}/grid-reference`,
+      headers: { cookie: 'crumb=' + crumbToken },
+      payload: {
         gridReferenceNumber: 'AB-12478975',
         crumb: crumbToken
-    }
+      }
     }
 
     const postResponse = await global.__SERVER__.inject(postOptions)
     expect(postResponse.statusCode).toBe(200)
     expect(postResponse.payload).toContain('First two characters should be letter following eight characters must be numbers')
-})
+  })
 
-it('store user response and redirect to planning-permission-summary: /planning-permission-summary', async () => {
+  it('store user response and redirect to planning-permission-summary: /planning-permission-summary', async () => {
     const postOptions = {
-    method: 'POST',
-    url: `${global.__URLPREFIX__}/grid-reference`,
-    headers: { cookie: 'crumb=' + crumbToken },
-    payload: {
+      method: 'POST',
+      url: `${global.__URLPREFIX__}/grid-reference`,
+      headers: { cookie: 'crumb=' + crumbToken },
+      payload: {
         gridReferenceNumber: 'AB12478975',
         crumb: crumbToken
-    }
+      }
     }
 
     const postResponse = await global.__SERVER__.inject(postOptions)
     expect(postResponse.statusCode).toBe(302)
     expect(postResponse.headers.location).toBe('planning-permission-summary')
-})
+  })
 })
