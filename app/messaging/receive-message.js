7dcb0e776d63c5ff8151de632de76f488e5ace5c
diya2214 <madihakhn@yahoo.com>
2022-07-20 18:42:03 +0100

    adding logging

--- app/messaging/receive-message.js
+++ app/messaging/receive-message.js
@@ -8,6 +8,7 @@ async function receiveMessage (messageId, config) {
   const messages = await receiver.receiveMessages(1, { maxWaitTimeInMs: 50000 })
   if (messages && messages.length) {
     result = messages[0].body
+    console.log(result,'result received')
     await receiver.completeMessage(messages[0])
   }
   await receiver.closeConnection()
