4cc548ab792931ae064c995535457b8513c83d7a
diya2214 <madihakhn@yahoo.com>
2022-07-28 13:07:52 +0100

    code smell fix

--- app/helpers/other-items-sizes.js
+++ app/helpers/other-items-sizes.js
@@ -2,6 +2,40 @@ const {
   WHOLE_NUMBER_REGEX
 } = require('./regex')
 
+const formatTempObject = (item, keyTitle, suffixValue, catagoryData) => {
+  const maxValue = catagoryData.inputLength === 4 ? 9999 : 9999999999
+
+  return { yarKey: item.item.replace(/[- ,)(]/g, ''), // Could add key to db list, to be used for populating yar?
+  type: 'number',
+  suffix: { text: suffixValue },
+  hint: {
+    text: `Grant amount: £${item.amount} ${item.unit}`
+  },
+  classes: `govuk-input--width-${catagoryData.inputLength}`,
+  label: {
+    text: item.item, 
+    classes: 'govuk-label--m'
+  },
+  validate: [
+    {
+      type: 'NOT_EMPTY',
+      error: `Enter ${keyTitle} ${catagoryData.errorType.toLowerCase()}`
+    },
+    {
+      type: 'REGEX',
+      regex: WHOLE_NUMBER_REGEX,
+      error: `${catagoryData.errorType} must be a whole number`
+    },
+    {
+      type: 'MIN_MAX',
+      min: 1,
+      max: maxValue ,
+      error: `${catagoryData.errorType} must be between 1-${maxValue}`
+    }
+  ]
+}
+}
+
 function suffixGenerator (unit) {
   // add correct suffix value to input field
   if (unit === 'per cubic metre') {
@@ -50,39 +84,8 @@ function formatOtherItems (request) {
             const suffixValue = suffixGenerator(item.unit)
             const keyTitle = keyGenerator(selectedCatagory.title)
             const catagoryData = getErrorUnitAndLength(listOfCatagories[catagory])
-            const maxValue = catagoryData.inputLength === 4 ? 9999 : 9999999999
-
-            // format object
-            const tempObject = {
-              yarKey: item.item.replace(/[- ,)(]/g, ''), // Could add key to db list, to be used for populating yar?
-              type: 'number',
-              suffix: { text: suffixValue },
-              hint: {
-                text: `Grant amount: £${item.amount} ${item.unit}`
-              },
-              classes: `govuk-input--width-${catagoryData.inputLength}`,
-              label: {
-                text: item.item, 
-                classes: 'govuk-label--m'
-              },
-              validate: [
-                {
-                  type: 'NOT_EMPTY',
-                  error: `Enter ${keyTitle} ${catagoryData.errorType.toLowerCase()}`
-                },
-                {
-                  type: 'REGEX',
-                  regex: WHOLE_NUMBER_REGEX,
-                  error: `${catagoryData.errorType} must be a whole number`
-                },
-                {
-                  type: 'MIN_MAX',
-                  min: 1,
-                  max: maxValue ,
-                  error: `${catagoryData.errorType} must be between 1-${maxValue}`
-                }
-              ]
-            }
+            
+            const tempObject = formatTempObject(item, keyTitle, suffixValue, catagoryData)
 
             returnArray.push(tempObject)
           }
