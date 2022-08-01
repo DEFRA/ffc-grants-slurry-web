d49b84230f85c52554cadba77b75709c5152daed
ogbk <ogbu.k.olu@gmail.com>
2022-08-01 23:58:17 +0100

    test getCheckDetailsModel()

--- test/integration/narrow/helpers/pageHelpers.test.js
+++ test/integration/narrow/helpers/pageHelpers.test.js
@@ -1,8 +1,11 @@
 describe('Page Helpers', () => {
   jest.mock('../../../../app/helpers/session')
-  const { getYarValue } = require('../../../../app/helpers/session')
+  const { getYarValue, setYarValue } = require('../../../../app/helpers/session')
 
-  const { handleConditinalHtmlData } = require('../../../../app/helpers/pageHelpers')
+  const {
+    handleConditinalHtmlData,
+    getCheckDetailsModel
+  } = require('../../../../app/helpers/pageHelpers')
 
   test('check handleConditinalHtmlData()', () => {
     let result
@@ -16,5 +19,81 @@ describe('Page Helpers', () => {
     result = handleConditinalHtmlData('input', 'mock-label-data', 'mock-yarKey', {})
     expect(result).toContain('<label class="govuk-label" for="mock-yarKey">')
     expect(result).toContain('<input class="govuk-input govuk-!-width-one-third" id="mock-yarKey" name="mock-yarKey" value="mock-yarValue">')
+  })
+
+  test('check getCheckDetailsModel()', () => {
+    let dict
+    getYarValue.mockImplementation((req, key) => (dict[key]))
+    setYarValue.mockImplementation((req, key, val) => { dict[key] = val })
+
+    dict = {
+      applying: true
+    }
+    expect(getCheckDetailsModel({}, {}, 'backUrl', 'nextUrl')).toEqual({
+      backUrl: 'backUrl',
+      nextUrl: 'nextUrl',
+      applying: true,
+      farmerDetails: {},
+      agentDetails: {}
+    })
+
+    dict = {
+      ...dict,
+      farmerDetails: {
+        firstName: 'Farmer',
+        lastName: 'FLastName',
+        emailAddress: 'farmer-email',
+        landlineNumber: 'farmer-landline',
+        address1: 'farmer-address1',
+        address2: 'farmer-address2',
+        town: 'farmer-town',
+        county: 'farmer-county',
+        postcode: 'farmer-postcode'
+      },
+      agentsDetails: {
+        firstName: 'Agent',
+        lastName: 'ALastName',
+        emailAddress: 'agent-email',
+        landlineNumber: 'agent-landline',
+        address1: 'agent-address1',
+        address2: 'agent-address2',
+        town: 'agent-town',
+        county: 'agent-county',
+        postcode: 'agent-postcode'
+      }
+    }
+    expect(getCheckDetailsModel({}, {}, 'backUrl', 'nextUrl')).toEqual({
+      backUrl: 'backUrl',
+      nextUrl: 'nextUrl',
+      applying: true,
+      farmerDetails: {
+        firstName: 'Farmer',
+        lastName: 'FLastName',
+        emailAddress: 'farmer-email',
+        landlineNumber: 'farmer-landline',
+        address1: 'farmer-address1',
+        address2: 'farmer-address2',
+        town: 'farmer-town',
+        county: 'farmer-county',
+        postcode: 'farmer-postcode',
+        name: 'Farmer FLastName',
+        contact: 'farmer-email<br/>farmer-landline',
+        address: 'farmer-address1<br/>farmer-address2<br/>farmer-town<br/>farmer-county<br/>farmer-postcode'
+      },
+      agentDetails: {
+        firstName: 'Agent',
+        lastName: 'ALastName',
+        emailAddress: 'agent-email',
+        landlineNumber: 'agent-landline',
+        address1: 'agent-address1',
+        address2: 'agent-address2',
+        town: 'agent-town',
+        county: 'agent-county',
+        postcode: 'agent-postcode',
+        name: 'Agent ALastName',
+        contact: 'agent-email<br/>agent-landline',
+        address: 'agent-address1<br/>agent-address2<br/>agent-town<br/>agent-county<br/>agent-postcode'
+      }
+    })
   })
 })
