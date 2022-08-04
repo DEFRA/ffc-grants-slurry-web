194eb1d50a2e8208751d4018de24afb71dc39e61
ogbk <ogbu.k.olu@gmail.com>
2022-08-05 00:01:21 +0100

    test checkInputError()

--- test/integration/narrow/helpers/errorSummaryHandlers.test.js
+++ test/integration/narrow/helpers/errorSummaryHandlers.test.js
@@ -8,7 +8,10 @@ describe('Get & Post Handlers', () => {
   })
   const { getQuestionAnswer } = require('../../../../app/helpers/utils')
 
-  const { validateAnswerField } = require('../../../../app/helpers/errorSummaryHandlers')
+  const {
+    validateAnswerField,
+    checkInputError
+  } = require('../../../../app/helpers/errorSummaryHandlers')
 
   test('check validateAnswerField()', async () => {
     let value = 'value'
@@ -37,5 +40,15 @@ describe('Get & Post Handlers', () => {
     expect(validateAnswerField(value, 'MAX_SELECT', details, {})).toBe(false)
 
     expect(validateAnswerField(value, 'DEFAULT_SELECT', details, {})).toBe(false)
+  })
+
+  test('check checkInputError()', () => {
+    let validate = [{ type: 'NOT_EMPTY', dependentKey: 'dep-yarKey' }]
+    expect(checkInputError(validate, false, {}, '')).toBe(undefined)
+
+    validate = [{ type: 'DEFAULT_SELECT', dependentKey: 'dep-yarKey' }]
+    expect(checkInputError(validate, true, { 'dep-yarKey': 'depYarkeyValue' }, '')).toEqual(
+      { type: 'DEFAULT_SELECT', dependentKey: 'dep-yarKey' }
+    )
   })
 })
