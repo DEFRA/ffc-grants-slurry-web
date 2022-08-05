5600fe0a352ac5df5bccb9515aa4a58e33756093
ogbk <ogbu.k.olu@gmail.com>
2022-08-05 14:53:11 +0100

    test customiseErrorText()

--- test/integration/narrow/helpers/errorSummaryHandlers.test.js
+++ test/integration/narrow/helpers/errorSummaryHandlers.test.js
@@ -8,10 +8,28 @@ describe('Get & Post Handlers', () => {
   })
   const { getQuestionAnswer } = require('../../../../app/helpers/utils')
 
+  jest.mock('../../../../app/helpers/session', () => {
+    const original = jest.requireActual('../../../../app/helpers/session')
+    return {
+      ...original,
+      getYarValue: jest.fn()
+    }
+  })
+  const { getYarValue } = require('../../../../app/helpers/session')
+
+  jest.mock('../../../../app/helpers/conditionalHTML')
+  const { getHtml } = require('../../../../app/helpers/conditionalHTML')
+
+  jest.mock('../../../../app/helpers/models')
+  const { getModel } = require('../../../../app/helpers/models')
+
   const {
     validateAnswerField,
-    checkInputError
+    checkInputError,
+    customiseErrorText
   } = require('../../../../app/helpers/errorSummaryHandlers')
+
+  let mockH
 
   test('check validateAnswerField()', async () => {
     let value = 'value'
@@ -49,6 +67,67 @@ describe('Get & Post Handlers', () => {
     validate = [{ type: 'DEFAULT_SELECT', dependentKey: 'dep-yarKey' }]
     expect(checkInputError(validate, true, { 'dep-yarKey': 'depYarkeyValue' }, '')).toEqual(
       { type: 'DEFAULT_SELECT', dependentKey: 'dep-yarKey' }
+    )
+  })
+
+  test('check customiseErrorText()', () => {
+    mockH = { view: jest.fn() }
+    getYarValue.mockReturnValue('mock-yar-value')
+    getHtml.mockReturnValue('mock-html')
+    getModel.mockReturnValue({ items: ['item1', 'item2'] })
+
+    let currentQuestion = {
+      yarKey: 'mock-yarKey',
+      type: 'multi-input',
+      conditionalKey: 'mock-condKey'
+    }
+    let errorList = [{ href: 'mock-yarKey', text: 'mock-href-text' }]
+    customiseErrorText('mock-value', currentQuestion, errorList, mockH, {})
+    expect(mockH.view).toHaveBeenCalledWith(
+      'page',
+      {
+        items: ['item1', 'item2'],
+        errorList: [{
+          href: 'mock-yarKey',
+          text: 'mock-href-text'
+        }]
+      })
+
+    getModel.mockReturnValue({ items: { item1: 'item1', item2: 'item2' } })
+    currentQuestion = {
+      ...currentQuestion,
+      type: 'mock-type'
+    }
+    mockH.view.mockClear()
+    customiseErrorText('mock-value', currentQuestion, errorList, mockH, {})
+    expect(mockH.view).toHaveBeenCalledWith(
+      'page',
+      {
+        items: {
+          item1: 'item1',
+          item2: 'item2',
+          errorMessage: { text: 'mock-href-text' }
+        },
+        errorList: [{
+          href: 'mock-yarKey',
+          text: 'mock-href-text'
+        }]
+      }
+    )
+
+    getModel.mockReturnValue({ items: { item1: 'item1', item2: 'item2' } })
+    errorList = [{ href: 'mock-another-yarKey', text: 'mock-another-href-text' }]
+    mockH.view.mockClear()
+    customiseErrorText('mock-value', currentQuestion, errorList, mockH, {})
+    expect(mockH.view).toHaveBeenCalledWith(
+      'page',
+      {
+        items: { item1: 'item1', item2: 'item2' },
+        errorList: [{
+          href: 'mock-another-yarKey',
+          text: 'mock-another-href-text'
+        }]
+      }
     )
   })
 })
