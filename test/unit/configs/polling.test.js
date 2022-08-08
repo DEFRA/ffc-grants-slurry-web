916a3afbfdb49e444a4c59183301870e0e2c7b60
ogbk <ogbu.k.olu@gmail.com>
2022-08-08 18:29:06 +0100

    test polling.js

---
+++ test/unit/configs/polling.test.js
@@ -0,0 +1,17 @@
+describe('polling.js', () => {
+  const {
+    POLLING_INTERVAL,
+    POLLING_RETRIES,
+    BACKEND_POLLING_HOST
+  } = process.env
+
+  const value = require('../../../app/config/polling')
+
+  test('check polling config', () => {
+    expect(value).toEqual({
+      interval: Number(POLLING_INTERVAL),
+      retries: Number(POLLING_RETRIES),
+      host: BACKEND_POLLING_HOST
+    })
+  })
+})
