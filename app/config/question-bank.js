7223a49740dda3e6a3c1e692aa865fdf4d033b09
Julian Taylor <julian.taylor@capgemini.com>
2022-08-12 21:03:17 +0100

    Wrapping <p> tag with classes not needed.

--- app/config/question-bank.js
+++ app/config/question-bank.js
@@ -576,7 +576,7 @@ const questionBank = {
             }],
             details: {
               summaryText: 'When is a store no longer fit for purpose?',
-              html: '<p class="govuk-list govuk-list--bullet">A store is no longer fit for purpose if it has reached the end of its design life and may be susceptible to leaks or failure.</p>'
+              html: 'A store is no longer fit for purpose if it has reached the end of its design life and may be susceptible to leaks or failure.'
             }
           },
           validate: [
@@ -767,7 +767,7 @@ const questionBank = {
             }],
             details: {
               summaryText: 'What is acidification?',
-              html: '<p class="govuk-list govuk-list--bullet">Acidification is the use of acid treatment to lower the pH value of slurry to stabilise ammonia emissions.</p>'
+              html: 'Acidification is the use of acid treatment to lower the pH value of slurry to stabilise ammonia emissions.'
             }
           },
           validate: [
