48dd7768c29a3347af916eeeb851a630613360dd
zavies97 <zacsdavies@sky.com>
2022-09-22 10:33:22 +0100

    Revert "Increased sonar coverage"
    This reverts commit 0156a1be5f5aef21084284691b75bc24d17b45ee.

--- app/helpers/handlers.js
+++ app/helpers/handlers.js
@@ -45,8 +45,8 @@ const sendContactDetailsToSenders = async (request, confirmationId) => {
       dimensionOrMetric: gapiService.metrics.CONFIRMATION,
       value: 'TIME'
     }
-    ]) // NOSONAR
-    console.log('Confirmation event sent') // NOSONAR
+    ])
+    console.log('Confirmation event sent')
   } catch (err) {
     console.log('ERROR: ', err)
   }
@@ -57,7 +57,7 @@ const setTitle = async (title, question, request) => {
     return {
       ...question,
       title: title.replace(SELECT_VARIABLE_TO_REPLACE, (_ignore, additionalYarKeyName) => (
-        formatUKCurrency(getYarValue(request, additionalYarKeyName) || 0) // NOSONAR
+        formatUKCurrency(getYarValue(request, additionalYarKeyName) || 0)
       ))
     }
   }
@@ -241,7 +241,7 @@ const showPostPage = (currentQuestion, request, h) => {
     currentQuestion = {
       ...currentQuestion,
       title: title.replace(SELECT_VARIABLE_TO_REPLACE, (_ignore, additionalYarKeyName) => (
-        formatUKCurrency(getYarValue(request, additionalYarKeyName) || 0) // NOSONAR
+        formatUKCurrency(getYarValue(request, additionalYarKeyName) || 0)
       ))
     }
   }
