028cf4557c1497d10e3eaabeda71563e50b75864
salfedev <sal.abuewilly@capgemini.com>
2023-09-26 09:51:56 +0100

    fix plugin

--- app/plugins/gapi.js
+++ app/plugins/gapi.js
@@ -1,5 +1,7 @@
 const Analytics = require('@defra/hapi-gapi/lib/analytics')
 const gapiService = require('../services/gapi-service')
+const { getYarValue } = require('../helpers/session')
+
 exports.plugin = {
   name: 'Gapi',
   /**
@@ -11,30 +13,26 @@ exports.plugin = {
   register: async (server, options) => {
     const analytics = new Analytics(options)
     server.decorate('request', 'ga', request => analytics.ga(request), { apply: true })
+
     server.ext('onPreResponse', async (request, h) => {
-      const response = request.response
-      const statusFamily = Math.floor(response.statusCode / 100)
-      if (statusFamily === 2 && response.variety === 'view' && !gapiService.isBlockDefaultPageView(request.url)) {
-        try {
+      try {
+        const response = request.response
+        const statusFamily = Math.floor(response.statusCode / 100)
+        if (statusFamily === 2 && response.variety === 'view' && !gapiService.isBlockDefaultPageView(request.url)) {
           await gapiService.sendGAEvent(request, {
-            name: gapiService.eventTypes.PAGEVIEW || 'pageview',
+            name: gapiService.eventTypes.PAGEVIEW,
             params: {
               page_path: request.route.path,
               page_title: request.route.fingerprint,
               host_name: request.info.hostname,
             }
           })
-          console.log('[ PAGEVIEW MATRIC SENT ]')
-        } catch (error) {
-          console.log(`[THIS IS GA ERROR: ${error}]`)
         }
-      }
-      if (statusFamily === 5) {
-        try {
+        if (statusFamily === 5) {
           await request.ga.event({ category: 'Exception', action: request.route.path, label: response.statusCode })
-        } catch (error) {
-          console.log(`[THIS IS GA ERROR: ${error}]`)
         }
+      } catch (error) {
+        console.log(`[THIS IS GA ERROR: ${error}]`)
       }
       return h.continue
     })
